bc2d524bb1fbcb479d54e98b9f399693e4d4ec7a
odox33 <anws3656@gmail.com>
2026-09-03 21:45:07 +0300

    Rename command and adjust user information reply
    Refactor arabic_id_command to id_arabic and update message format.

--- spr/modules/arabic_id.py
+++ spr/modules/arabic_id.py
@@ -2,22 +2,14 @@ from pyrogram import filters
 from pyrogram.types import Message
 from spr import spr
 
-
-@spr.on_message(filters.command(["ايدي", "الآيدي", "أيدي"], prefixes=["", "/", "#", "."]) & filters.group)
-async def arabic_id_command(_, message: Message):
-    user = message.from_user
+@spr.on_message(filters.command(["ايدي", "الآيدي"], prefixes=["", "/", "#", "."]))
+async def id_arabic(client, message: Message):
+    usr = message.from_user
     chat = message.chat
-    
-    text = (
+    await message.reply_text(
         f"**معلوماتك الشخصية:**\n"
-        f"• الاسم: {user.mention}\n"
-        f"• الآيدي: `{user.id}`\n\n"
-        f"**معلومات المجموعه:**\n"
-        f"• اسم المجموعة: {chat.title}\n"
-        f"• آيدي المجموعة: `{chat.id}`"
+        f"• الاسم: {usr.first_name}\n"
+        f"• الآيدي: `{usr.id}`\n\n"
+        f"**معلومات المحادثة:**\n"
+        f"• الآيدي: `{chat.id}`"
     )
-    
-    await message.reply_text(text)
-
-__MODULE__ = "Arabic ID"
-__HELP__ = "اكتب 'ايدي' في المجموعة ليقوم البوت بإرسال معلوماتك وآيدي المجموعة."
